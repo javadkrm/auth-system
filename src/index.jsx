c96b16a39f9e0cbc57ff1550cabdb4f069909e16
javad <javad@gmail.com>
2025-12-17 16:02:44 +0330

    pages added

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <UseRefEx/>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+
