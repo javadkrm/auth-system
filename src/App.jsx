176493f8a1ab69afd5ba8135ca485ae7dbb2d4c5
javad <javad@gmail.com>
2025-12-17 16:56:10 +0330

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import './App.css'
 import routes from './routes'
 import { useRoutes } from 'react-router-dom'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
     let router = useRoutes(routes)
-  return (
-    <div>
-        {router}
-    </div>
-  )
+    return (
+        <div className='app'>
+            <Navbar />
+            <div className='main container'>
+                {router}
+            </div>
+        </div>
+    )
 }
 
 export default App
